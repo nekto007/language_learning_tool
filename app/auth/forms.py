e0185e1f90b77010d23199108477d593515cf655
igorkorobko <nekto.korobko@gmail.com>
2025-04-18 18:24:11 +0300

    adding translate to elements register, login, dashboard

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,33 +1,33 @@
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, PasswordField, StringField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
-
+from flask_babel import lazy_gettext as _l
 from app.auth.models import User
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
+    username = StringField(_l('Username'), validators=[DataRequired()])
+    password = PasswordField(_l('Password'), validators=[DataRequired()])
+    remember_me = BooleanField(_l('Remember Me'))
+    submit = SubmitField(_l('Sign In'))
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=64)])
-    email = StringField('Email', validators=[DataRequired(), Email(), Length(max=120)])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=128)])
-    password2 = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Register')
+    username = StringField(_l('Username'), validators=[DataRequired(), Length(min=3, max=64)])
+    email = StringField(_l('Email'), validators=[DataRequired(), Email(), Length(max=120)])
+    password = PasswordField(_l('Password'), validators=[DataRequired(), Length(min=8, max=128)])
+    password2 = PasswordField(_l('Confirm Password'), validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField(_l('Register'))
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user:
-            raise ValidationError('Username already in use.')
+            raise ValidationError(_l('Username already in use.'))
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user:
-            raise ValidationError('Email already registered.')
+            raise ValidationError(_l('Email already registered.'))
 
 
 class RequestResetForm(FlaskForm):
@@ -37,22 +37,22 @@ class RequestResetForm(FlaskForm):
         Email(),
         Length(max=120)
     ])
-    submit = SubmitField('Request Password Reset')
+    submit = SubmitField(_l('Request Password Reset'))
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if not user:
-            raise ValidationError('There is no account with that email. You must register first.')
+            raise ValidationError(_l('There is no account with that email. You must register first.'))
 
 
 class ResetPasswordForm(FlaskForm):
     """Form for resetting a password after receiving a reset token."""
-    password = PasswordField('New Password', validators=[
+    password = PasswordField(_l('New Password'), validators=[
         DataRequired(),
         Length(min=8, max=128)
     ])
-    password2 = PasswordField('Confirm New Password', validators=[
+    password2 = PasswordField(_l('Confirm New Password'), validators=[
         DataRequired(),
         EqualTo('password')
     ])
-    submit = SubmitField('Reset Password')
+    submit = SubmitField(_l('Reset Password'))
