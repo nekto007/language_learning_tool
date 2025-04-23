6eabd0063a388a1397a55a3a1a255a34d5eb43c5
igorkorobko <nekto.korobko@gmail.com>
2025-04-24 01:12:42 +0300

    adding postfix service

---
+++ postfix.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+postconf -e "myhostname = mail.llt-english.com"
+postconf -e "mydomain = llt-english.com"
+postconf -e "myorigin = /etc/mailname"
+echo "llt-english.com" > /etc/mailname
+
+postconf -e "mydestination = "
+postconf -e "relayhost = "
+postconf -e "inet_interfaces = all"
+postconf -e "inet_protocols = ipv4"
+
+postconf -e "smtpd_tls_cert_file=/etc/letsencrypt/live/llt-english.com/fullchain.pem"
+postconf -e "smtpd_tls_key_file=/etc/letsencrypt/live/llt-english.com/privkey.pem"
+postconf -e "smtpd_use_tls=yes"
+postconf -e "smtp_tls_security_level = may"
+postconf -e "smtp_tls_note_starttls_offer = yes"
+
+exec /usr/sbin/postfix start-fg
