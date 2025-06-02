cad232dc85e74d545a2000495b80f4f98ca35648
igorkorobko <nekto.korobko@gmail.com>
2025-06-02 23:03:42 +0300

    улучшение работы раздела учеба.

--- app/words/forms.py
+++ app/words/forms.py
@@ -1,6 +1,6 @@
 from flask_babel import lazy_gettext as _l
 from flask_wtf import FlaskForm
-from wtforms import HiddenField, SelectField, StringField, SubmitField, TextAreaField
+from wtforms import BooleanField, HiddenField, SelectField, StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Length, Optional
 
 
@@ -13,13 +13,13 @@ class WordSearchForm(FlaskForm):
 
 
 class WordFilterForm(FlaskForm):
-    status = SelectField(_l('Status'), choices=[
-        (-1, _l('All')),
-        (0, _l('New')),
-        (1, _l('Learning')),
-        (2, _l('Review')),
-        (3, _l('Mastered'))
-    ], coerce=int, validators=[Optional()])
+    status = SelectField(_l('Статус'), choices=[
+        ('all', _l('Все')),
+        ('new', _l('Новые')),
+        ('learning', _l('Изучаемые')),
+        ('review', _l('На повторении')),
+        ('mastered', _l('Выученные'))
+    ], default='all', validators=[Optional()])
 
     letter = StringField(_l('Letter'), validators=[Optional()])
     book_id = SelectField(_l('Book'), coerce=int, validators=[Optional()])
@@ -35,7 +35,10 @@ class WordFilterForm(FlaskForm):
         books = [(0, _l('All Books'))]
         try:
             from app.utils.db import db
-            all_books = db.session.execute(db.select(Book)).scalars().all()
+            # Получаем книги отсортированные по названию
+            all_books = db.session.execute(
+                db.select(Book).order_by(Book.title.asc())
+            ).scalars().all()
             books.extend([(b.id, b.title) for b in all_books])
         except:
             pass
@@ -48,6 +51,8 @@ class TopicForm(FlaskForm):
     name = StringField(_l('Topic Name'), validators=[DataRequired(), Length(min=2, max=100)])
     description = TextAreaField(_l('Description'), validators=[Optional(), Length(max=500)])
     submit = SubmitField(_l('Save Topic'))
+    
+    # CSRF enabled by default for data modification forms
 
 
 class CollectionForm(FlaskForm):
@@ -60,6 +65,8 @@ class CollectionForm(FlaskForm):
     word_ids = HiddenField(_l('Selected Words'))
 
     submit = SubmitField(_l('Save Collection'))
+    
+    # CSRF enabled by default for data modification forms
 
 
 class CollectionFilterForm(FlaskForm):
@@ -68,9 +75,29 @@ class CollectionFilterForm(FlaskForm):
     search = StringField(_l('Search'), validators=[Optional(), Length(max=100)])
     submit = SubmitField(_l('Filter'))
 
+    class Meta:
+        csrf = False  # Disable CSRF for GET filter forms
+
     def __init__(self, *args, **kwargs):
         super(CollectionFilterForm, self).__init__(*args, **kwargs)
         from app.words.models import Topic
         # Динамическое заполнение списка тем
         topics = Topic.query.order_by(Topic.name).all()
         self.topic.choices = [('', _l('All Topics'))] + [(str(t.id), t.name) for t in topics]
+
+
+class AnkiExportForm(FlaskForm):
+    """Форма для экспорта в Anki"""
+    status = SelectField(_l('Статус слов'), choices=[
+        ('all', _l('Все слова')),
+        ('new', _l('Новые')),
+        ('learning', _l('Изучаемые')),
+        ('review', _l('На повторении')),
+        ('mastered', _l('Выученные'))
+    ], default='all', validators=[DataRequired()])
+    
+    include_audio = BooleanField(_l('Включить аудио произношение'), default=True)
+    
+    submit = SubmitField(_l('Экспортировать'))
+    
+    # CSRF enabled by default for export operations
