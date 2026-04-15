c07e89af026a2b56c394b17475be3bb77b090144
igorkorobko <nekto.korobko@gmail.com>
2026-04-15 18:24:14 +0300

    fix: merge remaining production alembic heads

---
+++ migrations/versions/a9b8c7d6e5f4_merge_production_heads.py
@@ -0,0 +1,25 @@
+"""Merge production Alembic heads into a single head.
+
+Revision ID: a9b8c7d6e5f4
+Revises: 03a5a0454960, dca40f9b45ee, f1e2d3c4b5a6
+Create Date: 2026-04-15
+"""
+
+
+# revision identifiers, used by Alembic.
+revision = 'a9b8c7d6e5f4'
+down_revision = (
+    '03a5a0454960',
+    'dca40f9b45ee',
+    'f1e2d3c4b5a6',
+)
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
